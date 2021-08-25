5cdcaa20b75560b8512d08734e2ee9b7f069d45b
Lior leizerovitch <liorle1997@gmail.com>
2021-08-25 18:18:36 +0300

    working add text note function

--- js/apps/keep/cmps/AddTextNote.jsx
+++ js/apps/keep/cmps/AddTextNote.jsx
@@ -1,9 +1,38 @@
+import { noteService } from "../services/note.service.js";
+
+export class AddTextNote extends React.Component {
+    state = {
+        info: {
+            text: ''
+        }
+    }
+    //type in props
+    handleChange = (ev) => {
+        const field = ev.target.name
+        const value = ev.target.value;
+        this.setState(prevState => ({ info: { ...prevState.info, [field]: value } }))
+
+    }
+
+    onSaveNote = (ev, noteInfo) => {
+        const noteType = this.props.type
+        const { loadNotes } = this.props
+        ev.preventDefault();
+        noteService.onSaveNote(noteType, noteInfo)
+            .then(loadNotes)
+
+    }
 
 
-export function AddTextNote({ type }) {
-    return (
-        <form className="add-note">
-            <input className="note-input" type={type} placeholder="Enter text"/>
-        </form>
-    )
+    render() {
+        const { type } = this.props
+        const { value } = this.state.info
+        return (
+            <form className="add-note" onSubmit={() => {
+                this.onSaveNote(event, this.state.info)
+            }} >
+                <input className="note-input" name="text" value={value} type={type} placeholder="Enter text" onChange={this.handleChange} />
+            </form>
+        )
+    }
 }
---
+++ js/apps/keep/cmps/NotesFilter.jsx
@@ -0,0 +1,25 @@
+
+
+export class NotesFilter extends React.Component {
+    state = {
+        filterBy: ''
+    }
+
+    handleChange = (ev) => {
+        const value = ev.target.value;
+        this.setState({ filterBy: value },()=>{this.props.onSetFilter(this.state.filterBy)})
+
+    }
+
+
+    render() {
+        const { filterBy } = this.state
+        const { onSetFilter } = this.props
+        return (
+            <form className="notes-filter" onSubmit={() => { onSetFilter(filterBy) }}>
+                <label htmlFor="filterBy">Search for notes</label>
+                <input className="filter-input" type="text" id="filterBy" name="filterBy" value={filterBy} placeholder="Search note" onChange={this.handleChange} />
+            </form>
+        )
+    }
+}
