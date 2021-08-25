18c48bb07e7b6f13cd71ffe5fbfa66bb4835a5d8
Lior leizerovitch <liorle1997@gmail.com>
2021-08-25 13:19:02 +0300

    fixed css and more app cleaning

---
+++ js/apps/book/pages/About.jsx
@@ -0,0 +1,14 @@
+const { NavLink, Route } = ReactRouterDOM
+
+
+export function About() {
+
+
+
+  return (
+    <section className="about">
+      <strong>We're all about books...</strong>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam repellat corporis accusantium, magni quasi sequi tempore laudantium quia vel temporibus aut aperiam. Temporibus possimus voluptate aliquam repellat laborum excepturi ducimus!</p>
+    </section>
+  )
+}
--- js/pages/app-about.jsx
+++ js/pages/app-about.jsx
@@ -11,30 +11,33 @@ export function About() {
         <section className="about">
             <strong>We are all about the sauce</strong>
             <p>Meet out team</p>
-            {/* <div className="team-card">
-                <img src="../../css/assets/imgs/lior.PNG" className="about-img" />
-                <div>
-                    <h4>Lior leizerovitch</h4>
-                    <p>Just created this meme editor go ahead have fun</p>
-                    <div className="socials">
-                        <a className="linked" href="#"></a>
-                        <a className="git" href="#"></a>
-                        <a className="face" href="#"></a>
+            <div className="cards">
+
+                <div className="team-card">
+                    <img src="../../css/assests/imgs/lior.PNG" className="about-img" />
+                    <div>
+                        <h4>Lior leizerovitch</h4>
+                        <p>Just created this meme editor go ahead have fun</p>
+                        <div className="socials">
+                            <a className="linked" href="#"></a>
+                            <a className="git" href="#"></a>
+                            <a className="face" href="#"></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="team-card">
+                    <img src="../../css/assests/imgs/tal.jpeg" className="about-img" />
+                    <div>
+                        <h4>Tal Ekroni</h4>
+                        <p>Just created this meme editor go ahead have fun</p>
+                        <div className="socials">
+                            <a className="linked" href="#"></a>
+                            <a className="git" href="#"></a>
+                            <a className="face" href="#"></a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="team-card">
-                <img src="../../css/assets/imgs/tal.jpg" className="about-img" />
-                <div>
-                    <h4>Tal Ekroni</h4>
-                    <p>Just created this meme editor go ahead have fun</p>
-                    <div className="socials">
-                        <a className="linked" href="#"></a>
-                        <a className="git" href="#"></a>
-                        <a className="face" href="#"></a>
-                    </div>
-                </div>
-            </div> */}
         </section>
         // <section>
 
