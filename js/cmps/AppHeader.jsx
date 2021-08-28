a9a4896dfaf63e3eb523880ba7be7fcd80898ee3
Lior leizerovitch <liorle1997@gmail.com>
2021-08-28 20:37:28 +0300

    added css changes to main page and responsivenes all around

--- js/cmps/AppHeader.jsx
+++ js/cmps/AppHeader.jsx
@@ -1,35 +1,41 @@
 const { NavLink, withRouter } = ReactRouterDOM
-import {eventBusService} from '../services/event-bus-service.js'
-import {mailService} from '../apps/mail/services/mail.service.js'
+import { eventBusService } from '../services/event-bus-service.js'
+import { mailService } from '../apps/mail/services/mail.service.js'
 class _AppHeader extends React.Component {
-state={
-    emailsCount:0
-}
+    state = {
+        emailsCount: 0,
+        isMenuOpen: false
+    }
 
 
-componentDidMount(){
-   eventBusService.on('emails-count',(emailsCount)=>{
-        this.setState({emailsCount})
-    })
-    let unread = mailService.getUnreadEmails()
-    eventBusService.emit('emails-count',unread.length)
- 
-}
+    componentDidMount() {
+        eventBusService.on('emails-count', (emailsCount) => {
+            this.setState({ emailsCount })
+        })
+        let unread = mailService.getUnreadEmails()
+        eventBusService.emit('emails-count', unread.length)
+
+    }
+    onOpenMenu = () => {
+        this.setState(prevState => ({ isMenuOpen: !prevState.isMenuOpen }))
+    }
 
 
     render() {
-
+        const { isMenuOpen } = this.state
         return (
             <section className="app-header">
+                
                 <h1 onClick={() => this.props.history.push('/')}>AppSus</h1>
-                <span>{this.state.emailsCount===0?'':this.state.emailsCount}</span>
-                <nav>
+                <span>{this.state.emailsCount === 0 ? '' : this.state.emailsCount}</span>
+                <nav className={isMenuOpen ? "menu-open" : ''}>
                     <NavLink exact to="/" ></NavLink>
                     <NavLink to="/about" ></NavLink>
                     <NavLink to="/book"></NavLink>
                     <NavLink to="/emails"></NavLink>
                     <NavLink to="/keep"></NavLink>
                 </nav>
+                <button className="menu-toggle-btn" onClick={this.onOpenMenu}>☰</button>
             </section>
         )
     }
